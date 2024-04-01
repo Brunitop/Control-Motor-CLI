098be0789bd22aea1e091641465ab51d296b16fd
Brunitop <68580588+Brunitop@users.noreply.github.com>
2024-04-01 12:00:30 -0700

    Update Program.cs

--- MotorApp/Program.cs
+++ MotorApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 
 namespace MotorApp
@@ -8,11 +8,13 @@ namespace MotorApp
         static int direccion = 0; // Variable para determinar la dirección en la que gira el abanico/motor
         static int frame = 0;
         static bool salir = false; // Variable para controlar si se debe salir del ciclo de animación
+        static readonly object consoleLock = new object(); // Objeto para sincronizar el acceso a la consola
         static ManualResetEvent pausaAnimacion = new ManualResetEvent(true); // Objeto para pausar y reanudar la animación
 
         static void Main(string[] args)
         {
             Thread animationThread = new Thread(MostrarAnimacion); // Thread para manejar la animación
+            Thread timeThread = new Thread(MostrarTiempo); // Thread para manejar el contador de tiempo
 
             int opcion;
             bool start = true;
@@ -20,14 +22,19 @@ namespace MotorApp
             {
                 Console.Clear();
                 MostrarMenu(); // Mostrar el menú inicial
-                
+
                 if(start){
                     animationThread.Start(); // Inicia el hilo de la animación después de configurar todo
+                    timeThread.Start(); // Inicia el contador del tiempo después de configurar todo
                     start = false;
                 }
-                pausaAnimacion.Set(); //Reanudar la animación
 
                 Console.Write("Opción: ");
+                desplegarTiempo();
+                desplegarFecha();
+
+                pausaAnimacion.Set(); // Reanudar la animación
+
                 opcion = LeerOpcion();
 
                 switch (opcion)
@@ -50,11 +57,12 @@ namespace MotorApp
                         Console.WriteLine("Opción no válida.");
                         break;
                 }
-                pausaAnimacion.Reset(); //Pausar la animacion
+                pausaAnimacion.Reset(); // Pausar la animación
             } while (!salir); // Repetir hasta que se elija la opción de salir
 
             // Espera a que el hilo de la animación termine antes de salir del programa
             animationThread.Join();
+            timeThread.Join();
 
             Console.Clear();
         }
@@ -78,11 +86,15 @@ namespace MotorApp
                 }
                 else
                 {
-                    Console.SetCursorPosition(0, 4); 
-                    Console.Write(new string(' ', Console.WindowWidth)); // Escribir espacios en blanco en toda la fila
-                    Console.SetCursorPosition(0, 4); 
-                    Console.WriteLine("Por favor, ingrese un número entero válido.");
-                    Console.Write("Opción: ");
+                    lock (consoleLock)
+                    {
+                        Console.SetCursorPosition(0, 5);
+                        Console.Write(new string(' ', Console.WindowWidth)); // Escribir espacios en blanco en toda la fila
+                        Console.SetCursorPosition(0, 5);
+                        Console.WriteLine("Por favor, ingrese un número entero válido.");
+                        Console.SetCursorPosition(0, 4);
+                        Console.Write("Opción: ");
+                    }
                 }
             }
         }
@@ -102,19 +114,64 @@ namespace MotorApp
                     frame = 3;
                 }
 
-                // Guardar la posición actual del cursor
-                int cursorLeft = Console.CursorLeft;
-                int cursorTop = Console.CursorTop;
+                lock (consoleLock)
+                {
+                    // Cambia la imagen del abanico según la dirección
+                    Console.SetCursorPosition(0, 5); // Establecer la posición de la animación
+                    Console.WriteLine(MotorAnimations.Abanico[frame]);
 
-                // Cambia la imagen del abanico según la dirección
-                Console.SetCursorPosition(0, 5); // Establecer la posición de la animación
-                Console.WriteLine(MotorAnimations.Abanico[frame]);
-
-                // Restaurar la posición del cursor
-                Console.SetCursorPosition(cursorLeft, cursorTop);
+                    // Restaurar la posición del cursor
+                    Console.SetCursorPosition(7, 4);
+                }
 
                 Thread.Sleep(250); // Retraso de 0.25 segundos para cambiar la imagen cada 0.25 segundos
                 frame += direccion;
+            }
+        }
+
+        static void MostrarTiempo()
+        {
+            while (!salir) // Continuar mientras no se elija la opción de salir
+            {
+                pausaAnimacion.WaitOne(); // Esperar a que se levante la pausa
+                desplegarTiempo();
+                Thread.Sleep(1000); // Retraso de 1 segundo
+            }
+        }
+
+        static void desplegarTiempo()
+        {
+            // Obtener hora y fecha actual del dispositivo
+            DateTime now = DateTime.Now;
+
+            lock (consoleLock)
+            {
+                // Cambiar la imagen de la hora
+                Console.SetCursorPosition(0, 18); // Establecer la posición de la animación
+                Console.Write(new string(' ', Console.WindowWidth)); // Escribir espacios en blanco en toda la fila
+                Console.SetCursorPosition(0, 18); // Establecer la posición de la animación
+                Console.WriteLine(now.ToString("HH:mm:ss"));
+
+                // Restaurar la posición del cursor
+                Console.SetCursorPosition(7, 4);
+            }
+        }
+
+        static void desplegarFecha()
+        {
+            // Obtener hora y fecha actual del dispositivo
+            DateTime now = DateTime.Now;
+
+            lock (consoleLock)
+            {
+                // Cambiar la imagen de la hora
+                Console.SetCursorPosition(0, 19); // Establecer la posición de la animación
+                Console.Write(new string(' ', Console.WindowWidth)); // Escribir espacios en blanco en toda la fila
+                Console.SetCursorPosition(0, 19); // Establecer la posición de la animación
+                Console.WriteLine(now.ToShortDateString());
+
+                // Restaurar la posición del cursor
+                Console.SetCursorPosition(7, 4);
             }
         }
     }
